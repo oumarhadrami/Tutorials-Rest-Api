01c55d2ded4845d7d15b01a003df1b126ae93f04
oumarhadrami <mohamedelhadramyoumar@gmail.com>
2021-03-06 23:50:04 +0000

    Update Cors Policy to allow all origins

--- src/main/java/com/mohamed/hadramy/springbootapp/config/CorsConfig.java
+++ src/main/java/com/mohamed/hadramy/springbootapp/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*");
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
     }
 }
 
